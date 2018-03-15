0b9e84ab340cc0180a30bd3f805ec35fe0cc2f2e
Reid Crawford <reidcrawford55@gmail.com>
2018-03-15 00:48:52 -0700

    got slide fade working

--- js/script.js
+++ js/script.js
@@ -9,9 +9,28 @@ $(document).ready(function(){
     s += '  <p> Project Manager /  Art Director</p>';
     s += '  <a href class="vcard">Download VCard</a>';
     s += '</div>';
- 	$('.content').fadeOut("slow");
- 	$('.content').fadeIn("slow"); 	
- 	$('.content').html(s);
+ 	// $('.content').fadeOut("slow");
+ 	// $('.content').fadeIn("slow");
+ 	$('.content').html(''); 	
+ 	
+ 	$(".content")
+  		.css('opacity', 0)
+  		// .fadeOut('3000')
+  		.slideUp('3000')
+  		// .append(s)
+  		.animate(
+    	{ opacity: 1 },
+    	{ queue: false, duration: '100' }
+  )
+  		// .fadeOut('3000')
+  		.slideDown('3000')
+  		.append(s)
+  		.animate(
+    	{ opacity: 1 },
+    	{ queue: false, duration: '100' }
+  );
+ 	
+
 
   })
 
