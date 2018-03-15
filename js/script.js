6b9c7342ef1b738ce80274697a33d901eb95aad3
Reid Crawford <reidcrawford55@gmail.com>
2018-03-15 00:57:27 -0700

    updated logic

--- js/script.js
+++ js/script.js
@@ -3,12 +3,10 @@ $(document).ready(function(){
   	$('.content').html('');
   })
   $('#home').on('click', function(clickEvent) {
-  	var s = '';
-    s += '<div class="content">';
+  	var s = ''
     s += '  <h2>Hello, I\'m John</h2>';
     s += '  <p> Project Manager /  Art Director</p>';
     s += '  <a href class="vcard">Download VCard</a>';
-    s += '</div>';
  	// $('.content').fadeOut("slow");
  	// $('.content').fadeIn("slow");
  	$('.content').html(''); 	
